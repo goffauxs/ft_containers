2d3135a522f9ebd7e956fa4c1f340767c7f8cfba
Simon Goffaux <sgoffaux@student.s19.be>
2022-01-25 11:38:28 +0100

    random_access_iterator and reverse_iterator should be done

--- random_access_iterator.hpp
+++ random_access_iterator.hpp
@@ -11,7 +11,7 @@ namespace ft
 		typedef typename ft::iterator<ft::random_access_iterator_tag, T>::difference_type	difference_type;
 		typedef typename ft::iterator<ft::random_access_iterator_tag, T>::pointer			pointer;
 		typedef typename ft::iterator<ft::random_access_iterator_tag, T>::reference			reference;
-	private:
+	protected:
 		pointer _ptr;
 	public:
 		// Default constructors
@@ -27,8 +27,65 @@ namespace ft
 		// Destructor
 		~RandIter() {}
 
+		// Member functions
+		pointer const& base() const { return this->_ptr; }
+
 		// Dereference operators
-		reference operator*() { return *this->_ptr; }
-		
+		reference operator*() const { return *this->_ptr; }
+		pointer operator->() const { return &(operator*()); }
+		reference operator[](difference_type n) const { return this->_ptr[n]; }
+
+		// Addition operator
+		RandIter operator+(difference_type n) const { return RandIter(this->_ptr + n); }
+
+		// Increment operators
+		RandIter& operator++() { ++this->_ptr; return *this; }
+		RandIter operator++(int) { RandIter tmp(*this); ++(*this); return tmp; }
+
+		// Advance operator
+		RandIter& operator+=(difference_type n) { this->_ptr += n; return *this; }
+
+		// Substraction operator
+		RandIter operator-(difference_type n) const { return RandIter(this->_ptr - n); }
+
+		// Decrement operators
+		RandIter& operator--() { --this->_ptr; return *this; }
+		RandIter operator--(int) { RandIter tmp(*this); --(*this); return tmp; }
+
+		// Retrocede operator
+		RandIter& operator-=(difference_type n) { this->_ptr -= n; return *this;}
 	};
+
+	// Relational operators
+	template <class Iterator1, class Iterator2>
+	bool operator==(const RandIter<Iterator1>& lhs, const RandIter<Iterator2>& rhs) { return lhs.base() == rhs.base(); }
+
+	template <class Iterator1, class Iterator2>
+	bool operator!=(const RandIter<Iterator1>& lhs, const RandIter<Iterator2>& rhs) { return lhs.base() != rhs.base(); }
+
+	template <class Iterator1, class Iterator2>
+	bool operator<(const RandIter<Iterator1>& lhs, const RandIter<Iterator2>& rhs) { return lhs.base() < rhs.base(); }
+
+	template <class Iterator1, class Iterator2>
+	bool operator<=(const RandIter<Iterator1>& lhs, const RandIter<Iterator2>& rhs) { return lhs.base() <= rhs.base(); }
+
+	template <class Iterator1, class Iterator2>
+	bool operator>(const RandIter<Iterator1>& lhs, const RandIter<Iterator2>& rhs) { return lhs.base() > rhs.base(); }
+
+	template <class Iterator1, class Iterator2>
+	bool operator>=(const RandIter<Iterator1>& lhs, const RandIter<Iterator2>& rhs) { return lhs.base() >= rhs.base(); }
+
+	// Addition operator overload
+	template <class Iterator>
+	RandIter<Iterator> operator+(typename RandIter<Iterator>::difference_type n, const RandIter<Iterator>& it)
+	{
+		return it + n;
+	}
+
+	// Substraction operator overload
+	template <class Iterator>
+	typename RandIter<Iterator>::difference_type operator-(const RandIter<Iterator>& lhs, const RandIter<Iterator>& rhs)
+	{
+		return lhs.base() - rhs.base();
+	}
 };
