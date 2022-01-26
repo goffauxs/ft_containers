eb26b9c03ce6565b060e9dfc2efd45210f5a7434
Simon Goffaux <sgoffaux@student.s19.be>
2022-01-26 15:52:32 +0100

    vector WIP

---
+++ utils.hpp
@@ -0,0 +1,29 @@
+#include "type_traits.hpp"
+#include "iterator_traits.hpp"
+
+namespace ft
+{
+	template <class InputIterator>
+	typename iterator_traits<InputIterator>::difference_type __distance(InputIterator __first, InputIterator __last, input_iterator_tag)
+	{
+		typename iterator_traits<InputIterator>::difference_type __n = 0;
+		while(__first != __last)
+		{
+			++__first;
+			++__n;
+		}
+		return __n;
+	}
+
+	template <class RandomAccessIterator>
+	typename iterator_traits<RandomAccessIterator>::difference_type __distance(RandomAccessIterator __first, RandomAccessIterator __last, random_access_iterator_tag)
+	{
+		return __last - __first;
+	}
+
+	template <typename InputIterator>
+	typename iterator_traits<InputIterator>::difference_type distance(InputIterator first, InputIterator last)
+	{
+		return ft::__distance(first, last, iterator_traits<InputIterator>::iterator_category());
+	}
+}
--- vector.hpp
+++ vector.hpp
@@ -1,10 +1,12 @@
 #include "random_access_iterator.hpp"
 #include "reverse_iterator.hpp"
+#include "type_traits.hpp"
+#include "utils.hpp"
 #include <memory>
 
 namespace ft
 {
-	template <class T, class Alloc = std::allocator<T> >
+	template <class T, class Alloc = std::allocator<T>>
 	class vector
 	{
 	public:
@@ -22,23 +24,121 @@ namespace ft
 		typedef 			std::size_t										size_type;
 	private:
 		allocator_type	_alloc;
-		pointer			_array;
-		size_type		_capacity;
-		size_type		_size;
+		pointer			_start;
+		pointer			_finish;
+		pointer			_end_of_storage;
 	public:
 		// Default constructor
 		explicit vector(const allocator_type& alloc = allocator_type())
-			: _alloc(alloc), _array(NULL), _capacity(0), _size(0) {}
+			: _alloc(alloc), _start(NULL), _finish(NULL), _end_of_storage(NULL) {}
 		
 		// Fill constructor
 		explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
 			: _alloc(alloc)
 		{
 			if (n < 0) throw std::out_of_range("vector");
-			this->_size = n;
-			this->_capacity = n;
-			this->_array = this->_alloc.allocate(n);
-			// TODO finish this function
+			this->_start = this->_alloc.allocate(n);
+			this->_finish = this->_start;
+			this->_end_of_storage = this->_start + n;
+			for (; n > 0; --n) this->_alloc.construct(this->_finish++, val);
 		}
+
+		// Range constructor
+		template <class InputIterator>
+		vector(InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type(),
+			ft::enable_if<!ft::is_integral<InputIterator>::value>::type* = NULL)
+		{
+			size_type n = ft::distance(first, last);
+			this->_start = this->_alloc.allocate(n);
+			this->_finish = this->_start;
+			this->_end_of_storage = this->_start + n;
+			for (; first != last; first++) this->_alloc.construct(this->_finish++, *first);
+		}
+
+		// Copy constructor
+		vector(const vector& other)
+			: _alloc(other._alloc)
+		{
+			size_type n = other.size();
+			this->_start = this->_alloc.allocate(n);
+			this->_finish = this->_start;
+			this->_end_of_storage = this->_start + n;
+			for (iterator it = other.begin(); it != other.end(); it++) this->_alloc.allocate(this->_finish++, *it);
+		}
+
+		// Assignement operator
+		vector& operator=(const vector& rhs)
+		{
+
+		}
+
+		/*
+		/ ITERATORS
+		*/
+
+		// begin
+		iterator		begin() 		{ return this->_start; }
+		const_iterator	begin() const	{ return this->_start; }
+
+		// end
+		iterator		end() 		{ return this->_finish; }
+		const_iterator	end() const { return this->_finish; }
+
+		// rbegin
+		reverse_iterator		rbegin()		{ return reverse_iterator(this->begin()); }
+		const_reverse_iterator	rbegin() const	{ return const_reverse_iterator(this->begin()); }
+
+		// rend
+		reverse_iterator		rend()			{ return reverse_iterator(this->_end()); }
+		const_reverse_iterator	rend() const	{ return const_reverse_iterator(this->end()); }
+
+		/*
+		/ CAPACITY
+		*/
+
+		// size
+		size_type size() const { return size_type(this->_finish - this->_start); }
+
+		// max_size
+		size_type max_size() const { return this->_alloc.max_size(); }
+
+		// resize
+		void resize(size_type n, value_type val = value_type())
+		{
+			if (n > this->size())
+				
+			else if (n < this->size())
+
+		}
+
+		// capacity
+		size_type capacity() const { return size_type(this->_end_of_storage - this->_start); }
+
+		// empty
+		bool empty() const { return this->size() == 0; }
+
+		// reserve
+		void reserve(size_type n)
+		{
+			if (n > this->max_size())
+				throw std::length_error("vector::reserve");
+			if (this->capacity() > n)
+				return;
+			const size_type old_capacity = this->_size();
+
+		}
+
+		/*
+		/ MODIFIERS
+		*/
+
+		template <class InputIterator>
+		void assign(InputIterator first, InputIterator last,
+			ft::enable_if<!ft::is_integral<InputIterator>::value>::true* = NULL)
+		{
+			
+		}
+
+
 	};
 }
