9a46ee0e0ad38c8f32ac9f0026065dbb3073b9b1
Simon Goffaux <sgoffaux@student.s19.be>
2022-01-28 16:00:17 +0100

    vector.insert() definitions WIP

--- vector.hpp
+++ vector.hpp
@@ -52,7 +52,7 @@ namespace ft
 			this->_start = this->_alloc.allocate(n);
 			this->_finish = this->_start;
 			this->_end_of_storage = this->_start + n;
-			for (; first != last; first++) this->_alloc.construct(this->_finish++, *first);
+			for (; first != last; first++, this->_finish++) this->_alloc.construct(this->_finish, *first);
 		}
 
 		// Copy constructor
@@ -63,7 +63,7 @@ namespace ft
 			this->_start = this->_alloc.allocate(n);
 			this->_finish = this->_start;
 			this->_end_of_storage = this->_start + n;
-			for (iterator it = other.begin(); it != other.end(); it++) this->_alloc.allocate(this->_finish++, *it);
+			for (iterator it = other.begin(); it != other.end(); it++, this->_finish++) this->_alloc.construct(this->_finish, *it);
 		}
 
 		// Assignement operator
@@ -122,10 +122,19 @@ namespace ft
 		{
 			if (n > this->max_size())
 				throw std::length_error("vector::reserve");
-			if (this->capacity() > n)
-				return;
-			const size_type old_capacity = this->_size();
+			if (n > this->capacity())
+			{
+				iterator old_start = this->begin();
+				iterator old_end = this->end();
+				size_type old_capacity = this->capacity();
 
+				this->_start = this->_alloc.allocate(n);
+				this->_finish = this->_start;
+				this->_end_of_storage = this->_start + n;
+				for (iterator it = old_start; it != old_end; it++, this->_finish++) this->_alloc.construct(this->_finish, *it);
+				for (; old_end != old_start; old_end--) this->_alloc.destroy(&(*old_end));
+				this->_alloc.deallocate(&(*old_start), old_capacity);
+			}
 		}
 
 		/*
@@ -139,6 +148,33 @@ namespace ft
 			
 		}
 
+		// insert
+		iterator insert(iterator position, const value_type& val)
+		{
+			
+		}
 
+		void insert(iterator position, size_type n, const value_type& val)
+		{
+			if (n == 0)
+				return ;
+			if (this->size() + n > this->capacity())
+				this->reserve(this->size() + n);
+			iterator end_pos = position + n;
+			for (iterator it = position; it != end_pos; it++, end_pos++)
+			{
+				this->_alloc.construct(&(*end_pos), *it);
+				this->_alloc.destroy(&(*it));
+			}
+			for (size_type i = 0; i < n; i++)
+				this->_alloc.construct((&(*position) + i), val);
+		}
+
+		template <class InputIterator>
+		void insert(iterator position, InputIterator first, InputIterator last,
+			ft::enable_if<ft::is_integral<InputIterator>::value>::type* = NULL)
+		{
+			
+		}
 	};
 }
